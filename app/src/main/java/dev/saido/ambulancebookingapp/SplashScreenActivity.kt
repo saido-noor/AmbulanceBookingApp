af18a614a74b93ede9c95a6669ea22af0eae137c
saido-noor <saidoomarnoor@gmail.com>
2022-07-18 11:05:23 +0300

    added login

---
+++ app/src/main/java/dev/saido/ambulancebookingapp/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package dev.saido.ambulancebookingapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+    }
+}
